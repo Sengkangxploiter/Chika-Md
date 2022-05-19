65bf66b0982a38ed994b74690f1f1e4dec519479
Sengkangxploiter <105027770+Sengkangxploiter@users.noreply.github.com>
2022-05-20 02:04:38 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @lucifer_xrifan
+› Base Ori @sengkangxploiter
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281395330156','6281395330156','6281395330156'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281395330156'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281395330156' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Lucifer_Xrifan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'SengkangXploiter-BOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Musrifan' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/RiyAPIs'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = 'https://youtube.com/channel/UC6gnmY0LztLwx-VpzEtWkzw'
+global.ig = 'https://instagram.com/lucifer_xrifan'
+global.mygc = 'https://chat.whatsapp.com/B5zZHo8SgYC2mknhtOeHDp'
+global.myweb = 'https://SengkangXploiter-XRifan.sengkangxploite.repl.co'
+global.email = ' sengkangxploiter@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© SengkangXploiter-BOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'lucifer_xrifan' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -62,7 +62,7 @@ global.veloriy = fs.readFileSync('./image/chika.mp4')
 
 //—————「 Set Image Welcome 」—————//
 //terserah mau ubah apa nggak, menurut gua jangan di ubah!
-global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
+global.BgWelcLeav = 'https://i.postimg.cc/vHwSVj48/Pics-Art-06-19-03-43-55.jpg'
 
 //—————「 Set Random Image Menu 」—————//
 //gausah di apa² in!
@@ -77,7 +77,7 @@ global.prefa = ['','!','.','🐦','🐤','🗿']
 
 //—————「 Set Simbol 」—————//
 //terserah mai ubah atau nggak, note : tanda ' gausah di hapus!
-global.sp = '⭔'
+global.sp = '👻'
 
 //—————「 Set Message 」—————//
 //terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
